b0692f9b1397fa48ebc373c86601ce9a90f7a4e9
Lucas-Sasaki <lucas_sasaki1@hotmail.com>
2024-03-25 21:25:36 -0300

    Atomações

---
+++ RPA Challenge/Desafio.py
@@ -0,0 +1,47 @@
+from selenium import webdriver as opcoesSelenium
+from selenium.webdriver.common.by import By
+import pyautogui as tempoEspera
+from openpyxl import load_workbook
+
+#Abrir site formulário
+navegador = opcoesSelenium.Chrome()
+navegador.get("https://rpachallenge.com/?lang=EN")
+tempoEspera.sleep(5)
+
+#Abrir aqrquivo Excel
+nomeArquivo = "challenge.xlsx"
+planilhaDados = load_workbook(nomeArquivo)
+sheetSelecionada = planilhaDados["Sheet1"]
+
+#Ler linha a linha do excel
+for linha in range(2,len(sheetSelecionada["A"]) + 1):
+    firstName = sheetSelecionada['A%s' % linha].value
+    lastName = sheetSelecionada['B%s' % linha].value
+    companyName = sheetSelecionada['C%s' % linha].value
+    roleInCompany = sheetSelecionada['D%s' % linha].value
+    adress = sheetSelecionada['E%s' % linha].value
+    email = sheetSelecionada['F%s' % linha].value
+    phoneNumber = sheetSelecionada['G%s' % linha].value
+
+    #Verifica se a célula está vazia
+    if firstName is None:
+        break
+
+    #Preencher formulário
+    navegador.find_element(By.XPATH, '//*[@ng-reflect-name="labelFirstName"]').send_keys(firstName)
+    tempoEspera.sleep(2)
+    navegador.find_element(By.XPATH, '//*[@ng-reflect-name="labelLastName"]').send_keys(lastName)
+    tempoEspera.sleep(2)
+    navegador.find_element(By.XPATH, '//*[@ng-reflect-name="labelCompanyName"]').send_keys(companyName)
+    tempoEspera.sleep(2)
+    navegador.find_element(By.XPATH, '//*[@ng-reflect-name="labelRole"]').send_keys(roleInCompany)
+    tempoEspera.sleep(2)
+    navegador.find_element(By.XPATH, '//*[@ng-reflect-name="labelAddress"]').send_keys(adress)
+    tempoEspera.sleep(2)
+    navegador.find_element(By.XPATH, '//*[@ng-reflect-name="labelEmail"]').send_keys(email)
+    tempoEspera.sleep(2)
+    navegador.find_element(By.XPATH, '//*[@ng-reflect-name="labelPhone"]').send_keys(phoneNumber)
+    tempoEspera.sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/app-root/div[2]/app-rpa1/div/div[2]/form/input').click()
+
+print("Processo finalizado")
